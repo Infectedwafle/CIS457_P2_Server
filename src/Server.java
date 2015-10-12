380ae14e0fd51d4281b2b6fff247f04c654977e2
Jon Powers <powersjo@mail.gvsu.edu>
2015-10-12 14:47:15 -0400

    added planning for server

--- src/Server.java
+++ src/Server.java
@@ -3,20 +3,33 @@ import java.net.*;
 
 class Server {
 	public static void main(String args[]) throws Exception {
-		DatagramSocket serverSocket = new DatagramSocket(9876);
-		while (true) {
-			byte[] receiveData = new byte[1024];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData,
-					receiveData.length);
-			serverSocket.receive(receivePacket);
-			String message = new String(receiveData);
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			System.out.println("Got message: " + message);
-			byte[] sendData = message.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData,
-					sendData.length, IPAddress, port);
-			serverSocket.send(sendPacket);
-		}
+	  DatagramSocket serverSocket = new DatagramSocket(9876);
+	  while (true) {
+	    byte[] receiveData = new byte[1024];
+	    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	    serverSocket.receive(receivePacket);
+	    String message = new String(receiveData);
+	    InetAddress IPAddress = receivePacket.getAddress();
+	    int port = receivePacket.getPort();
+	    System.out.println("Got message: " + message);
+	    //Need to check the message.
+	    /*
+	    if (!checkTypeClass()){
+	      // Send the client the RCODE that the request is not
+	      // supported.
+	    }
+	    // else we send the request to a root name server. 
+	    */
+	    byte[] sendData = message.getBytes();
+	    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+	    serverSocket.send(sendPacket);
+	  }
+	}
+	/* This method checks the type and class of the request.
+	*  If the type is other than A and the class is other than
+	*  IN the method should return false;
+	*/
+	public static boolean checkTypeClass(){
+	  return false;
 	}
 }
